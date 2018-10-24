7235f3cc97d74d141943c81168d3934b0077487b
Eliot <43924629+Eliot-M@users.noreply.github.com>
2018-10-24 16:48:45 +0200

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -5,12 +5,35 @@ import pandas as pd # To manipulate df
 import requests # To get page content
 from lxml import html # To access html tree
 
+# --- Get all index pages --- #
 
+# Get page content
+page = requests.get ('xxx')
+htmltree = html.fromstring(page.content)
+
+# Get main elements
+link = htmltree.xpath('//div[@class="xxx"]/a/@href')
+size = htmltree.xpath('//div[@class="xxx"]/text()')
+
+# Get values from strings
+size_num = [int(re.sub(' drinks', '', s)) for s in size] # get the total number of drinks on each category
+
+# Create the new list
+v_link= []
+
+# Get all links, and then all possible pages to create all full links
+for i,l in enumerate(link):
+    for value in range(int(((size_num[i]/10) // 1 + 1))): # 10 drinks per page
+        new_link = l + "?page=" + str(value+1) # Enumerate begin at 0 and pages list at 1
+        v_link.append(new_link)
+        
+        
+        
 # --- Scrap a page --- #
 
 def getDfRecipe(pagelink):
     '''
-    Goal: Get main informations (Cocktail Name, Ingredient & Quantity, Recipe) from a cocktail web page.
+    Goal: Get main informations (Drinks Names, Ingredient & Quantity, Recipe) from a recipe web page.
     Input: url link
     Output: Dataframe - Cocktail recipe (1 row by ingredient)
     To do: Raise an error if link not in link with the website
